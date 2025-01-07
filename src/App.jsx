f003b07d7a01e83655834449b4ccdf6b5d90c2c7
Yoga_Naden <143375989+Nadenbogeee@users.noreply.github.com>
2025-01-07 11:58:06 +0700

    FINAL

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Input } from "@nextui-org/react";
+import NavbarComponent from "./components/NavbarComponent";
+import HeroSection from "./components/HeroSection";
+import ProductPage from "./components/ProductPage";
+
+function App() {
+  return (
+    <>
+      {/* //line of component */}
+      {/* //line of pages */}
+      {/* //line of component */}
+      {/* //line of pages */}
+      <NavbarComponent />
+      <HeroSection />
+      <ProductPage />
+      {/* <Input label="Email" placeholder="Enter your email" type="text" className="border-2 border-red-500" /> */}
+    </>
+  );
+}
+
+export default App;
